205dd06e9aff0c9ada6e9db900eb3d3dbed60836
Ryan Emerle <Ryan_Emerle@comcast.com>
2020-10-20 18:01:44 -0400

    Update

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@
 rm -rf ./vendor
 mkdir ./vendor
 cd vendor
-git clone git@github.com:hzeller/rpi-rgb-led-matrix.git
+git clone git://github.com/hzeller/rpi-rgb-led-matrix.git
 git checkout 0a4f8f9a9
 cd rpi-rgb-led-matrix.git
 make
