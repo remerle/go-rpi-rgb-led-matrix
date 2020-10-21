8a9cf0cbfa45299b40161dbe31b857ef566f852c
remerl200 <ryan_emerle@comcast.com>
2020-10-21 10:52:18 -0400

    Update

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-chmod 755 .
-rm -rf ./vendor
-mkdir ./vendor
-cd vendor
+chmod 755 "${DIR}"
+rm -rf "${DIR}/vendor"
+mkdir "${DIR}/vendor"
+cd "${DIR}/vendor" || exit
 git clone git://github.com/hzeller/rpi-rgb-led-matrix.git
 git checkout 0a4f8f9a9
-cd rpi-rgb-led-matrix
+cd "${DIR}/vendor/rpi-rgb-led-matrix" || exit
 make
